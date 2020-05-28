44025bcf7d60c118a760d38e7f06b609eaf8b470
linkin-park <giridharan.arun123@gmail.com>
2020-05-29 03:09:18 +0530

    Modify chatdev.ts

--- src/db/chatdev.ts
+++ src/db/chatdev.ts
@@ -1,13 +1,14 @@
 import NodeCouchDb from "node-couchdb";
-import { dbconfig} from "./config";
+import { dbconfig } from "./config";
 import { v4 } from "uuid";
 import { Chat } from "./protocol";
+import { rejects } from "assert";
 
 const couch = new NodeCouchDb({
-  auth: { 
-    user: dbconfig.user, 
-    pass: dbconfig.pass 
-  }
+  auth: {
+    user: dbconfig.user,
+    pass: dbconfig.pass,
+  },
 });
 
 export function couchInsert(chat: Chat) {
@@ -17,7 +18,7 @@ export function couchInsert(chat: Chat) {
       datetime: chat.at,
       from: chat.from,
       to: chat.to,
-      message: chat.message
+      message: chat.message,
     })
     .then(
       ({ data, headers, status }) => {
@@ -25,8 +26,8 @@ export function couchInsert(chat: Chat) {
         // headers is an object with all response headers
         // status is statusCode number
       },
-      err => {
-        console.error(err)
+      (err) => {
+        console.error(err);
         console.error(`Something went wrong
               from : ${chat.from}
               to:${chat.to}
@@ -35,4 +36,52 @@ export function couchInsert(chat: Chat) {
         // ...or err.code=EDOCCONFLICT if document with the same id already exists
       }
     );
+}
+
+export async function couchRetrieveOn(
+  dateInNano: number,
+  channel: string = "coffeesh0p"
+): Promise<Array<Chat>> {
+  console.log(`what date? ${new Date(dateInNano)}`)
+  const prevDate = new Date(dateInNano);
+  prevDate.setDate(prevDate.getDate() - 1);
+  const nextDate = new Date(dateInNano);
+  nextDate.setDate(nextDate.getDate() + 1);
+
+  const mongoQuery = {
+    selector: {
+      datetime: {
+        $gt: prevDate.getTime() / 1000,
+        $lt: nextDate.getTime() / 1000,
+      },
+      to: {
+        $eq: channel,
+      },
+    },
+  };
+  const parameters = {};
+
+  const datas: Array<Chat> = [];
+ return couch.mango(dbconfig.db, mongoQuery, parameters).then(
+    ({ data, headers, status }) => {
+      // data is json response
+      // headers is an object with all response headers
+      // status is statusCode number
+      console.debug("Queried Mongo Query",mongoQuery)
+      // console.debug("db return Intended",data)
+      data.docs.forEach(({ datetime, from, to, message }) => {
+        datas.push({ at: datetime, from, to, message });
+      });
+      return datas;
+    },
+    (err: any) => {
+      // either request error occured
+      // ...or err.code=EDOCMISSING if document is missing
+      // ...or err.code=EUNKNOWN if statusCode is unexpected
+      throw err;
+    }
+  ).catch((err: any) => {
+     console.debug('Something funky happened!' , err)
+     return err
+    });
 }
